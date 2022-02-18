b7eb2df20de5aa9c17f41a6eb444c3ed851274d8
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-02-18 18:55:05 +0100

    covoiturage

--- src/main/java/dev/controllers/ReservationCovoiturageController.java
+++ src/main/java/dev/controllers/ReservationCovoiturageController.java
@@ -1,14 +1,17 @@
 package dev.controllers;
 
+import dev.dto.CreerReservationCovoiturageDto;
+import dev.dto.ModifierReservationCovoiturageDto;
 import dev.entites.reservation.ReservationCovoiturage;
 import dev.services.ReservationCovoiturageService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("reservation/covoiturage")
+@RequestMapping("resa/covoiturage")
 public class ReservationCovoiturageController {
 
     private ReservationCovoiturageService covoiturageService;
@@ -17,35 +20,52 @@ public class ReservationCovoiturageController {
         this.covoiturageService = covoiturageService;
     }
 
-    @GetMapping("{id_utilisateur}")
+    @GetMapping
+    public List<ReservationCovoiturage> listerToutesReservations(@RequestParam Integer start, @RequestParam Integer size){
+        return this.covoiturageService.listerToutesReservations(PageRequest.of(start,size));
+    }
+
+    @GetMapping("passager/{id_utilisateur}")
     public List<ReservationCovoiturage> afficherReservations(@PathVariable String id_utilisateur){
         return this.covoiturageService.afficherReservations(Integer.parseInt(id_utilisateur));
     }
 
-    @PostMapping("reserver")
-    public ResponseEntity<?> reserverCovoiturage(@RequestBody ReservationCovoiturage reservationCovoiturage){
+    @GetMapping("{id_resa}")
+    public ResponseEntity<?> afficherUneReservation(@PathVariable String id_resa){
         try{
-            return ResponseEntity.ok(this.covoiturageService.reserverCovoiturage(reservationCovoiturage));
-        }catch(Exception e){
+            return ResponseEntity.ok(this.covoiturageService.afficherUneReservation(Integer.parseInt(id_resa)));
+            } catch (Exception e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> reserverCovoiturage(@RequestBody CreerReservationCovoiturageDto resa){
+        try{
+            return ResponseEntity.ok(this.covoiturageService.reserverCovoiturage(resa));
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @DeleteMapping("{id_resa}")
-    public ResponseEntity<?> supprimerReservationCovoiturage(@RequestParam String id_resa){
+    public ResponseEntity<?> supprimerReservationCovoiturage(@PathVariable String id_resa){
         try{
             this.covoiturageService.supprimerReservationCovoiturage(Integer.parseInt(id_resa));
             return ResponseEntity.ok().build();
         }catch (Exception e){
+            e.printStackTrace();
             return ResponseEntity.notFound().build();
         }
     }
 
-    @PutMapping("modifier")
-    public ResponseEntity<?> modifierResa(@RequestBody ReservationCovoiturage resa){
+    @PutMapping
+    public ResponseEntity<?> modifierResa(@RequestBody ModifierReservationCovoiturageDto resa){
         try{
             return ResponseEntity.ok(this.covoiturageService.modifierReservationCovoiturage(resa));
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/java/dev/services/ReservationCovoiturageService.java
+++ src/main/java/dev/services/ReservationCovoiturageService.java
@@ -1,7 +1,14 @@
 package dev.services;
 
+import dev.dto.CreerReservationCovoiturageDto;
+import dev.dto.ModifierReservationCovoiturageDto;
+import dev.entites.AnnonceCovoiturage;
+import dev.entites.Utilisateur;
 import dev.entites.reservation.ReservationCovoiturage;
+import dev.repositories.AnnonceCovoiturageRepository;
 import dev.repositories.ReservationCovoiturageRepository;
+import dev.repositories.UtilisateurRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,29 +17,72 @@ import java.util.List;
 public class ReservationCovoiturageService {
 
     private ReservationCovoiturageRepository reservationCovoiturageRepository;
+    private AnnonceCovoiturageRepository annonceCovoiturageRepository;
+    private UtilisateurRepository utilisateurRepository;
 
-
-    public ReservationCovoiturageService(ReservationCovoiturageRepository reservationCovoiturageRepository) {
+    public ReservationCovoiturageService(ReservationCovoiturageRepository reservationCovoiturageRepository, AnnonceCovoiturageRepository annonceCovoiturageRepository, UtilisateurRepository utilisateurRepository) {
         this.reservationCovoiturageRepository = reservationCovoiturageRepository;
+        this.annonceCovoiturageRepository = annonceCovoiturageRepository;
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    private void verifierNbPlaces(AnnonceCovoiturage annonce) throws Exception {
+        if(annonce.getNbPlaces() <= this.reservationCovoiturageRepository.calculerNbPlacesReservees(annonce.getId())){
+            throw new Exception("Plus de places disponibles sur ce covoiturage");
+        }
     }
 
     public List<ReservationCovoiturage> afficherReservations(Integer id_utilisateur) {
         return this.reservationCovoiturageRepository.findAllById(List.of(id_utilisateur));
     }
 
-    public ReservationCovoiturage reserverCovoiturage(ReservationCovoiturage reservationCovoiturage) {
-        return this.reservationCovoiturageRepository.save(reservationCovoiturage);
+    public ReservationCovoiturage reserverCovoiturage(CreerReservationCovoiturageDto resa) throws Exception {
+        AnnonceCovoiturage annonce = this.annonceCovoiturageRepository
+                .findById(resa.getIdCovoiturage())
+                .orElseThrow(Exception::new);
+        Utilisateur collab = this.utilisateurRepository
+                .findById(resa.getIdCollaborateur())
+                .orElseThrow(Exception::new);
+
+        this.verifierNbPlaces(annonce);
+
+        ReservationCovoiturage nouvelleResa = new ReservationCovoiturage();
+        nouvelleResa.setAnnonceCovoiturage(annonce);
+        nouvelleResa.setPassager(collab);
+
+        return this.reservationCovoiturageRepository.save(nouvelleResa);
     }
 
     public void supprimerReservationCovoiturage(Integer id_resa){
         this.reservationCovoiturageRepository.deleteById(id_resa);
     }
 
-    public ReservationCovoiturage modifierReservationCovoiturage(ReservationCovoiturage resa) throws Exception{
-        this.reservationCovoiturageRepository
-                .findById(resa.getId())
+    public ReservationCovoiturage modifierReservationCovoiturage(ModifierReservationCovoiturageDto nouvelleResa) throws Exception{
+        ReservationCovoiturage resa = this.reservationCovoiturageRepository
+                .findById(nouvelleResa.getIdResa())
                 .orElseThrow(Exception::new);
-        this.supprimerReservationCovoiturage(resa.getId());
-        return this.reserverCovoiturage(resa);
+
+        Utilisateur pax = this.utilisateurRepository
+                        .findById(nouvelleResa.getIdPassager())
+                                .orElseThrow(Exception::new);
+
+        AnnonceCovoiturage annonce = this.annonceCovoiturageRepository
+                        .findById(nouvelleResa.getIdCovoiturage())
+                                .orElseThrow(Exception::new);
+
+        this.verifierNbPlaces(annonce);
+
+        resa.setPassager(pax);
+        resa.setAnnonceCovoiturage(annonce);
+        return this.reservationCovoiturageRepository.save(resa);
+    }
+
+    public ReservationCovoiturage afficherUneReservation(Integer id_resa) throws Exception {
+        return this.reservationCovoiturageRepository.findById(id_resa)
+                .orElseThrow(Exception::new);
+    }
+
+    public List<ReservationCovoiturage> listerToutesReservations(PageRequest pr) {
+        return this.reservationCovoiturageRepository.findAll(pr).toList();
     }
 }
