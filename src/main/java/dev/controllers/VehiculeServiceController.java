9ceaccf9f6a735b6bd2e09431781f3ec4f54f938
Vincent Lo <vincent.loreau4@gmail.com>
2022-02-21 11:56:53 +0100

    modifier véhicule service + creer vehicule service (service + controller)

--- src/main/java/dev/controllers/VehiculeServiceController.java
+++ src/main/java/dev/controllers/VehiculeServiceController.java
@@ -2,13 +2,19 @@ package dev.controllers;
 
 import java.util.List;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.dto.vehiculeService.VehiculeServiceListeDto;
+import dev.entites.VehiculeService;
 import dev.services.VehiculeServiceService;
 
 @RestController
@@ -26,6 +32,17 @@ public class VehiculeServiceController {
 	public List<VehiculeServiceListeDto> listerVehiculesService(@RequestParam Integer page,
 			@RequestParam Integer size) {
 		return this.vehiculeServiceService.afficherVehiculesService(PageRequest.of(page, size));
+	}
+
+	@PostMapping
+	public VehiculeService creerVehiculeService(@RequestBody VehiculeService vehiculeService) {
+		return this.vehiculeServiceService.creerVehiculeService(vehiculeService);
+	}
+
+	@PutMapping
+	public ResponseEntity<?> modifierVehiculeService(@RequestBody VehiculeService vehiculeService)
+			throws NotFoundException {
+		return ResponseEntity.ok(this.vehiculeServiceService.modifierVehiculeService(vehiculeService));
 	}
 
 }
