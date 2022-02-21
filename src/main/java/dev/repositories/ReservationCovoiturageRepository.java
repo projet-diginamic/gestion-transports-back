b4fbc659aca9cef1c26f3d6f407c293598feb482
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-02-21 12:23:58 +0100

    MapStruct & resa covoit dto

--- src/main/java/dev/repositories/ReservationCovoiturageRepository.java
+++ src/main/java/dev/repositories/ReservationCovoiturageRepository.java
@@ -1,5 +1,6 @@
 package dev.repositories;
 
+import dev.dto.ReservationCovoiturageSimpleDto;
 import dev.entites.Utilisateur;
 import dev.entites.reservation.ReservationCovoiturage;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -18,4 +21,7 @@ public interface ReservationCovoiturageRepository extends JpaRepository<Reservat
     @Query("SELECT r FROM ReservationCovoiturage r WHERE r.passager.id=:id ")
     List<ReservationCovoiturage> listerReservationsParUtilisateur(@Param("id") Integer id);
 
+    List<ReservationCovoiturage> findByPassagerIdAndAnnonceCovoiturageDateHeureDepartGreaterThanEqual(Integer id, LocalDateTime date);
+
+    List<ReservationCovoiturage> findByPassagerIdAndAnnonceCovoiturageDateHeureDepartLessThan(Integer id, LocalDateTime date);
 }
