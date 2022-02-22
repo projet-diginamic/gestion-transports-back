9d26d9bee43dd48ca0a32412855b5143c9e4d0f5
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-02-22 12:38:07 +0100

    modif req POST annonceC

--- src/main/java/dev/services/AnnonceCovoiturageService.java
+++ src/main/java/dev/services/AnnonceCovoiturageService.java
@@ -1,12 +1,12 @@
 package dev.services;
 
+import dev.dto.AnnonceCovoiturageDto;
 import dev.dto.reservation.covoiturage.ReqCovoit;
 import dev.entites.AnnonceCovoiturage;
 import dev.exception.CovoiturageCompletException;
 import dev.exception.DateDepasseeException;
 import dev.exception.NotFoundException;
-import dev.repositories.AnnonceCovoiturageRepository;
-import dev.repositories.ReservationCovoiturageRepository;
+import dev.repositories.*;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +21,20 @@ class AnnonceCovoiturageService {
     private AnnonceCovoiturageRepository annonceCovoiturageRepository;
     private ReservationCovoiturageRepository reservationCovoiturageRepository;
     private ReservationCovoiturageService reservationCovoiturageService;
+    private UtilisateurRepository utilisateurRepository;
+    private AdresseDepartRepository adresseDepartRepository;
+    private AdresseArriveeRepository adresseArriveeRepository;
+    private VehiculeCovoiturageRepository vehiculeCovoiturageRepository;
 
-    public AnnonceCovoiturageService(AnnonceCovoiturageRepository annonceCovoiturageRepository, ReservationCovoiturageRepository reservationCovoiturageRepository, ReservationCovoiturageService reservationCovoiturageService){
+    public AnnonceCovoiturageService(AnnonceCovoiturageRepository annonceCovoiturageRepository, ReservationCovoiturageRepository reservationCovoiturageRepository, ReservationCovoiturageService reservationCovoiturageService, UtilisateurRepository utilisateurRepository, AdresseDepartRepository adresseDepartRepository, AdresseArriveeRepository adresseArriveeRepository, VehiculeCovoiturageRepository vehiculeCovoiturageRepository){
         super();
         this.annonceCovoiturageRepository = annonceCovoiturageRepository;
         this.reservationCovoiturageRepository = reservationCovoiturageRepository;
         this.reservationCovoiturageService = reservationCovoiturageService;
+        this.utilisateurRepository = utilisateurRepository;
+        this.adresseDepartRepository = adresseDepartRepository;
+        this.adresseArriveeRepository = adresseArriveeRepository;
+        this.vehiculeCovoiturageRepository = vehiculeCovoiturageRepository;
     }
 
     private void verifierNbPlaces(AnnonceCovoiturage annonce) throws CovoiturageCompletException {
@@ -44,8 +52,23 @@ class AnnonceCovoiturageService {
                 .orElseThrow(NotFoundException::new);
     }
 
-    public AnnonceCovoiturage publierAnnonce(AnnonceCovoiturage annonce) {
-        return this.annonceCovoiturageRepository.save(annonce);
+    public AnnonceCovoiturage publierAnnonce(AnnonceCovoiturageDto dto) {
+        AnnonceCovoiturage a = new AnnonceCovoiturage();
+        a.setOrganisateur(this.utilisateurRepository
+                .findById(dto.getOrganisateur())
+                .orElseThrow(NotFoundException::new));
+
+        a.setAdresseDepart(this.adresseDepartRepository
+                .save(dto.getAdresseDepart()));
+
+        a.setAdresseArrivee(this.adresseArriveeRepository
+                .save(dto.getAdresseArrivee()));
+
+        a.setDateHeureDepart(dto.getDateHeureDepart());
+        a.setNbPlaces(dto.getNbPlaces());
+        a.setVehicule(this.vehiculeCovoiturageRepository
+                .save(dto.getVehicule()));
+        return this.annonceCovoiturageRepository.save(a);
     }
 
     public AnnonceCovoiturage supprimerCovoiturage (Integer id) throws NotFoundException, DateDepasseeException{
