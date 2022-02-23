d7e03be711a8c8f341943ff8a67a42f1f8ae3e18
MrTrnr <marietournier.pro@gmail.com>
2022-02-23 11:31:48 +0100

    ajout lister véhicules pour collaborateur

--- src/main/java/dev/repositories/VehiculeServiceRepository.java
+++ src/main/java/dev/repositories/VehiculeServiceRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import dev.dto.vehiculeService.VehiculeServiceListeDto;
+import dev.dto.vehiculeService.VehiculeServiceListeDtoCollaborateur;
 import dev.entites.VehiculeService;
 
 @Repository
@@ -18,6 +19,9 @@ public interface VehiculeServiceRepository extends JpaRepository<VehiculeService
 	@Query("select new dev.dto.vehiculeService.VehiculeServiceListeDto(v.id, v.immatriculation, v.marque, v.modele, v.photo, v.categorie.nom) from VehiculeService v "
 			+ "inner join v.categorie c ")
 	List<VehiculeServiceListeDto> listerVehicules(Pageable pageable);
+
+	@Query("select new dev.dto.vehiculeService.VehiculeServiceListeDtoCollaborateur(v.id, v.marque, v.modele, v.immatriculation, v.photo) from VehiculeService v ")
+	List<VehiculeServiceListeDtoCollaborateur> listerVehiculesCollaborateur(Pageable pageable);
 
 	@Query("select new dev.dto.vehiculeService.VehiculeServiceListeDto"
 			+ " (v.id, v.immatriculation, v.marque, v.modele, v.photo, v.categorie.nom) from VehiculeService v inner join v.categorie c where v.marque=:marque")
